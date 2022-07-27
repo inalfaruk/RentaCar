cde241de61c7db6356576b0fb70a15d8ea84e5f0
farukinal16 <farukinal16@gmail.com>
2022-07-27 17:47:15 +0300

    pc

---
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Concrete
+{
+    public class UserOperationClaim
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string OperationClaimId { get; set; }
+
+
+    }
+}
